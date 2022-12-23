98f00aca45a46de7bad68dd0eaaf4c6f5d7e87fc
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-12-23 10:11:03 +0300

    feat: add 02-numbers solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -112,9 +112,13 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scalarVector = x1 * x2 + y1 * y2;
+  const modulesVectorA = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const modulesVectorB = Math.sqrt(x2 ** 2 + y2 ** 2);
+  return Math.acos(scalarVector / (modulesVectorA * modulesVectorB));
 }
+
 
 /**
  * Returns a last digit of a integer number.
